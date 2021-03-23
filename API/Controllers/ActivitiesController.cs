b8420aa125187e38cae58a681f61b41b0ea5f095
krasni <borota.igor@gmail.com>
2021-03-23 16:04:53 +0100

    Privremeno

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,28 +5,22 @@ using Microsoft.AspNetCore.Mvc;
 using Persistence;
 using System;
 using Microsoft.EntityFrameworkCore;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-
-    public ActivitiesController(DataContext context)
-    {
-      _context = context;
-    }
-
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await _context.Activities.ToListAsync();
+        return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return await _context.Activities.FindAsync(id);
+       return Ok();
     }    
   }
 }
