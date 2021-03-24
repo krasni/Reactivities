5718f3303f16101593776aaf33ac6362075250fe
Borota Igor <borota.igor@gmail.com>
2021-03-24 20:10:18 +0100

    Ajmo

---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Menu } from 'semantic-ui-react';
+
+export default function NavBar() {
+  return (
+    <Menu inverted fixed='top'>
+      <Container>
+        <Menu.Item header>
+          
+        </Menu.Item>
+      </Container>
+    </Menu>
+  )
+}
