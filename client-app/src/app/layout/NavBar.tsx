b00e7b2dde05bdea13240c888c6a8bfdf04b5389
Borota Igor <borota.igor@gmail.com>
2021-03-25 22:01:43 +0100

    Yeah

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Container, Menu } from 'semantic-ui-react';
+import { Button, Container, Menu } from 'semantic-ui-react';
 
 export default function NavBar() {
   return (
     <Menu inverted fixed='top'>
       <Container>
         <Menu.Item header>
-          
+          <img src="/assets/logo.png" alt="logo" style={{marginRight: 10}}/>
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name = 'Activities' />
+        <Menu.Item>
+          <Button positive content='Create Activity' />
         </Menu.Item>
       </Container>
     </Menu>
---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+export default function ActivityDetails() {
+  return (
+    <Card>
+      
+    </Card>
+  )
+}
